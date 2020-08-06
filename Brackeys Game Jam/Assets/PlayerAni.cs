8622bbeec46b1af62b2734276515c832c7e5d8ee
tyhan216 <tyriese508@gmail.com>
2020-08-06 17:39:05 -0400

    Player plays walk and idle on command.

---
+++ Brackeys Game Jam/Assets/PlayerAni.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAni : MonoBehaviour
+{
+    PlayerMovement play;
+    Animator anime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        play = GetComponent<PlayerMovement>();
+        anime = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (play.PlayerCanMove)
+        {
+            if(Input.GetAxisRaw("Horizontal") > 0.2 || Input.GetAxisRaw("Horizontal") < -0.2)
+            {
+                anime.Play("Walk");
+            }
+            else if(Input.GetAxisRaw("Horizontal") <= 0.2 && Input.GetAxisRaw("Horizontal") >= -0.2)
+            {
+                anime.Play("Idle");
+            }
+        }
+    }
+}
