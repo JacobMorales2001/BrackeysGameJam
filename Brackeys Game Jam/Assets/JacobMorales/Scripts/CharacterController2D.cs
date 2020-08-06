1134707e06ab43581e8349739ebbdb1a68237ed2
tyhan216 <tyriese508@gmail.com>
2020-08-06 19:16:28 -0400

    All animations except grab and throw are setup

--- Brackeys Game Jam/Assets/JacobMorales/Scripts/CharacterController2D.cs
+++ Brackeys Game Jam/Assets/JacobMorales/Scripts/CharacterController2D.cs
@@ -13,10 +13,12 @@ public class CharacterController2D : MonoBehaviour
 	[SerializeField] private Collider2D m_CrouchDisableCollider = default;		// A collider that will be disabled when crouching
 
 	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-	private bool m_Grounded= default;            // Whether or not the player is grounded.
+	[HideInInspector]
+	public bool m_Grounded= default;            // Whether or not the player is grounded.
 	private float m_BigDrop = default;			// Whether or not the player was going down
 	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
-	private Rigidbody2D m_Rigidbody2D = default;
+	[HideInInspector]
+	public Rigidbody2D m_Rigidbody2D = default;
 	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
 	private Vector3 m_Velocity = Vector3.zero;
 
