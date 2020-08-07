dd499b2eb635131f050c7c574299b92fc2673faa
ImranB <imranbharadia@gmail.com>
2020-08-07 16:55:29 -0400

    Update
    Updated design of first level.
    Created button and door combo for level end.
    Created Goal prefab.

---
+++ Brackeys Game Jam/Assets/ImranBharadia/Scripts/ibButtonScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ibButtonScript : MonoBehaviour
+{
+    public GameObject endDoor;
+
+    private Animator buttonAnim, doorAnim;
+
+
+    void Start()
+    {
+        buttonAnim = GetComponent<Animator>();
+        doorAnim = endDoor.GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        buttonAnim.SetBool("ButtonHit", true);
+    }
+
+    public void ButtonPressed()
+    {
+        if (doorAnim.GetBool("ButtonPressed") == false)
+        {
+            doorAnim.SetBool("ButtonPressed", true);
+        }
+        else return;
+    }
+}
