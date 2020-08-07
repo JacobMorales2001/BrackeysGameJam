9f720a908bb2a6279fb5ebafa6cf14d8a31b8dd3
MarquisMc <marquismccann56@gmail.com>
2020-08-07 19:59:27 -0400

    cut rewind enemy and added camera shake and enemy death sound

---
+++ Brackeys Game Jam/Assets/MarquisMcCann/Scripts/CameraShake.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public IEnumerator ShakeWithTime(float duration, float magnitude)
+    {
+        Vector3 originalPos = transform.localPosition; // Original location of the camera
+
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-.5f, .5f) * magnitude;
+            float y = Random.Range(-.5f, .5f) * magnitude;
+
+            transform.localPosition = new Vector3(x, y, originalPos.z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localPosition = originalPos;
+    }
+
+    public IEnumerator ShakeWithBool(bool canShake, float magnitude)
+    {
+        Vector3 originalPos = transform.localPosition; // Original location of the camera
+
+        if (canShake)
+        {
+            float x = Random.Range(-.5f, .5f) * magnitude;
+            float y = Random.Range(-.5f, .5f) * magnitude;
+
+            transform.localPosition = new Vector3(x, y, originalPos.z);
+
+            yield return null;
+        }
+        else
+        {
+            transform.localPosition = originalPos;
+        }
+    }
+}
